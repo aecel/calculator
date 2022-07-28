8d5055d46703ceca219f6ef92f13023d26a173c1
aecel <aecelacuna@gmail.com>
2022-07-29 00:04:32 +0800

    Fix bugs of operations and rounds to 2 decimal places

--- js-file.js
+++ js-file.js
@@ -1,70 +1,92 @@
 const operate = (operation, num1, num2) => {
     if (operation == "add") {
-        return num1 + num2;
+        return Number(num1) + Number(num2);
     } else if (operation == "subtract") {
         return num1 - num2;
     } else if (operation == "multiply") {
         return num1 * num2;
     } else if (operation == "divide") {
-        return num1 / num2;
+        if (num2 == 0) {
+            return "Nah man";
+        } else {
+            return Math.round((num1 / num2)*100)/100;
+        }
     }
 }
 
-// Lights up a button when it is pressed
+
 const isClicked = (e) => {
+
+    // Lights up a button when it is pressed
     e.target.classList.toggle("pressedButton");
-    if (e.target.textContent == "AC") {
-        presentDisplay.textContent = "";
+    setTimeout(() => { e.target.classList.toggle("pressedButton"); }, 150);
+
+    if (e.target.textContent == "AC") {     // If AC is pressed
+        presentDisplay.textContent = 0;
         pastDisplay.textContent = "";
         num1 = undefined;
         num2 = undefined;
-        op = false;
-        prevButton = "";
+        operation = undefined;
+        prevButton = undefined;
         console.log("CLEAR ALL")
-    } else if (e.target.textContent == "+/-") {
+    } else if (e.target.textContent == "+/-") {     // If +/- is pressed
         console.log("NEG")
         prevButton = "neg";
-    } else if (e.target.textContent == "DEL") {
+    } else if (e.target.textContent == "DEL") {     // If DEL is pressed
         console.log("DELETE")
         prevButton = "delete";
-    } else if (e.target.classList.contains("num")) {
+    } else if (e.target.classList.contains("num")) {        // If a number is pressed
         if (presentDisplay.textContent == 0 || prevButton == "operation") {
             presentDisplay.textContent = e.target.textContent;
         } else {
             presentDisplay.textContent += e.target.textContent;
         }
         prevButton = "num";
-    } else if (e.target.classList.contains("operation")) {
-        op = true;
+    } else if (e.target.classList.contains("operation")) {      // If an operation is pressed
 
-        if (prevButton == "equals") {
-            pastDisplay.textContent = num1 + " " + e.target.textContent;;
-        } else {
+        if (prevButton == "operation") {
+            // Do nothing
+        } else if (prevButton == "equals") {
+            pastDisplay.textContent = num1 + " " + e.target.textContent;
+            presentDisplay.textContent = "";
+            operation = e.target.id;
+        } else if (num1 == undefined) {
             num1 = presentDisplay.textContent;
-            pastDisplay.textContent += num1 + " " + e.target.textContent;
+            pastDisplay.textContent = num1 + " " + e.target.textContent;
+            presentDisplay.textContent = "";
+            operation = e.target.id;
+        } else if (prevButton == "num") {
+            num2 = presentDisplay.textContent;
+            const answer = operate(operation, num1, num2);
+            pastDisplay.textContent = answer + " " + e.target.textContent;
+            presentDisplay.textContent = "";
+            num1 = answer;
+            num2 = undefined;
+            operation = e.target.id;
         }
-        operation = e.target.id;
-        console.log(`OPERATION: ${operation}, NUM1: ${num1}`);
+
         prevButton = "operation";
-    } else if (e.target.textContent == "=") {
-        if (op = true) {
+    } else if (e.target.textContent == "=") {       // If = is pressed
+        if (prevButton == "equals") {
+            // Do nothing
+        } else if ((prevButton == "num") && (operation)) {
             num2 = presentDisplay.textContent;
             pastDisplay.textContent += " " + num2 + " " + e.target.textContent;
             const answer = operate(operation, num1, num2);
             presentDisplay.textContent = answer;
 
-            console.log(`OPERATION: ${operation}, NUM2: ${num2}`)
-
-            num1 = answer;
+            if (typeof answer == "number") {        // Check if answer is not equal to "Nah man"
+                num1 = answer;
+            } else {
+                num1 = undefined;
+            }
             num2 = undefined;
-            op = false;
+            operation = undefined;
             prevButton = "equals";
         }
-        console.log("EQUALS");
-        console.log(`num1 = ${num1}, num2 = ${num2}`);
+
     }
-    // presentDisplay.textContent += e.target.textContent;
-    setTimeout(() => { e.target.classList.toggle("pressedButton"); }, 150);
+    
 }
 
 const buttons = document.querySelectorAll(".button");
@@ -76,6 +98,7 @@ let num2 = undefined;
 let op = false;
 let operation = undefined;
 let prevButton = undefined;
+let prevOperation = undefined;
 presentDisplay.textContent = 0;
 buttons.forEach((button) => {
     button.addEventListener("click", isClicked);
